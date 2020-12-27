dcf1a53c13803ebe150fd1a443dd604a84259aab
Allyson <allysonlarkindesign@gmail.com>
2020-12-27 12:42:53 +0000

    installed Axios and weather API

--- src/javascript.js
+++ src/javascript.js
@@ -1,1 +1,6 @@
 alert("Greetings!");
+let apiKey = "29a44231d6c7563fcc1740ef29d751ed";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+

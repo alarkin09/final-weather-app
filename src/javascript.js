63d4ca029370dc976a7e235fff2ec36ebb0a36a0
Allyson <allysonlarkindesign@gmail.com>
2021-01-01 21:11:37 +0000

    JS: Data within current temperature function configured

--- src/javascript.js
+++ src/javascript.js
@@ -1,13 +1,18 @@
 alert("Greetings!");
 
 function displayTemperature(response){
-    console.log(response.data.main.temp);
+    console.log(response.data);
     let temperatureElement = document.querySelector("#temperature");
     temperatureElement.innerHTML = Math.round(response.data.main.temp);
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = response.data.name;
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML = response.data.weather[0].description;
+    let humidityElement = document.querySelector("#humidity");
+    humidityElement.innerHTML = response.data.main.humidity;
+    let windElement = document.querySelector("#wind");
+    windElement.innerHTML = response.data.wind.speed;
 }
-
 let apiKey = "29a44231d6c7563fcc1740ef29d751ed";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
 

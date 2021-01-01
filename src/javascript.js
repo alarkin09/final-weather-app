6780e6ab2fbbdc5a40e2f04a46b56734eb003cab
Allyson <allysonlarkindesign@gmail.com>
2021-01-01 20:46:49 +0000

    JS: current city and temperature updated

--- src/javascript.js
+++ src/javascript.js
@@ -1,6 +1,15 @@
 alert("Greetings!");
+
+function displayTemperature(response){
+    console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = response.data.name;
+}
+
 let apiKey = "29a44231d6c7563fcc1740ef29d751ed";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=San Francisco&appid=${apiKey}&units=metric`;
 
 console.log(apiUrl);
-
+axios.get(apiUrl).then(displayTemperature);

14b590f3ceb0cdb3d3cca4b797a836be7494ae8b
Allyson <allysonlarkindesign@gmail.com>
2021-01-04 16:21:09 +0100

    CSS Color Updates and JS forecast issue fixed

--- src/javascript.js
+++ src/javascript.js
@@ -49,6 +49,7 @@ function displayTemperature(response){
 
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
+forecastElement.innerHTML = "";
   let forecast = null; 
  
   for (let index = 0; index < 6; index ++) {
